018f2f5efe8a80260dbb0b577373a3507ce96aa9
ZSJie <41783643+1217861302@users.noreply.github.com>
2022-11-18 10:29:19 +0800

    Add files via upload
    This is just a flv.js practice project,upload it only for practice how to use GitHub.

---
+++ rtspVideo/src/components/videoPlay.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="video-box">
+        <video controls="controls" class="demo-video" ref="player" muted @dblclick="fullScreen"></video>
+    </div>
+</template>
+   
+<script>
+import flvjs from 'flv.js'
+export default {
+    name: 'Home',
+    components: {},
+    data() {
+        return {
+            player: null,
+            loading: true
+        }
+    },
+    mounted() {
+        this.playVideo()
+    },
+    watch: {
+        rtsp: {
+            handler: function (val) {
+                if (val) {
+                    if (this.player) {
+                        this.player.unload()
+                        this.player.destroy()
+                        this.player = null
+                        this.loading = true
+                    }
+                    this.$nextTick(() => {
+                        this.playVideo()
+                    })
+                }
+            },
+            immediate: true
+        }
+    },
+    methods: {
+        fullScreen() {
+            if (this.$refs.player.requestFullScreen) {
+                this.$refs.player.requestFullScreen()
+            } else if (this.$refs.player.mozRequestFullScreen) {
+                this.$refs.player.mozRequestFullScreen()
+            } else if (this.$refs.player.webkitRequestFullScreen) {
+                this.$refs.player.webkitRequestFullScreen()
+            }
+        },
+        playVideo() {
+            const time1 = new Date().getTime()
+            if (flvjs.isSupported()) {
+                let video = this.$refs.player
+                if (video) {
+                    this.player = flvjs.createPlayer({
+                        type: 'flv',
+                        isLive: true,
+                        url: `ws://192.168.0.110:8199/rtsp/1/cnRzcDovL3dvd3phZWMyZGVtby5zdHJlYW1sb2NrLm5ldC92b2QvbXA0OkJpZ0J1Y2tCdW5ueV8xMTVrLm1wNA==`
+
+                    })
+                    this.player.attachMediaElement(video)
+                    try {
+                        this.player.load()
+                        this.player.play().then(() => {
+                            console.log(new Date().getTime() - time1)
+                            this.loading = false
+                        })
+                    } catch (error) {
+                        console.log(error)
+                    }
+                }
+            }
+        }
+    },
+    beforeDestroy() {
+        if (this.player) {
+            this.player.unload()
+            this.player.destroy()
+            this.player = null
+        }
+    }
+}
+</script>
+<style>
+.video-box {
+    width: 100%;
+    height: 100%;
+}
+
+video {
+    width: 100%;
+    height: 100%;
+    object-fit: fill;
+}
+
+video::-webkit-media-controls-play-button {
+    display: none;
+}
+
+video::-webkit-media-controls-current-time-display {
+    display: none;
+}
+
+video::-webkit-media-controls-timeline {
+    display: none;
+}
+</style>
+   
+   
---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <videoPlay></videoPlay>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import videoPlay from './components/videoPlay.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    videoPlay
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
